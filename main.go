7be1f6ec42f1b7d4bbbec1127c2d2f2e3bdb5f5b
HaizorWill <thelines6@gmail.com>
2025-11-12 22:55:51 +0300

    Wire it all up

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/HaizorWill/goctl-agent/core"
+)
+
+func main() {
+
+	app := core.NewApplication()
+	signal.Notify(
+		app.ShouldClose,
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGHUP,
+		syscall.SIGQUIT,
+	)
+	go func() {
+		sig := <-app.ShouldClose
+		fmt.Printf("\nReceived a signal %v, stopping...", sig)
+		time.Sleep(2 * time.Second)
+		app.Stop()
+		os.Exit(0)
+	}()
+	app.Run()
+}
